a2e7debdf8a37434fa3f3a63309b9696c4b7c1d4
Justin Thomas <thomascjustin@gmail.com>
2018-12-24 19:52:39 -0500

    Set up views and urls for users

--- student_gradebook_project/users/views.py
+++ student_gradebook_project/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Student, Teacher
 
 # Create your views here.
+def student(request):
+    context = {
+        'student': Student
+    }
+
+    return render(request, 'users/student.html', context)
+
+def teacher(request):
+    context = {
+        'students': Student.objects.all(),
+        'teacher': Teacher
+    }
+
+    return render(request, 'users/teacher.html', context)
