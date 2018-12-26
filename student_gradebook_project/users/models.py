935d2c84fa91ae82533d059f8fa4d0980ab50d51
Justin Thomas <thomascjustin@gmail.com>
2018-12-26 15:49:48 -0500

    Add grade dropdown options when entering students

--- student_gradebook_project/users/models.py
+++ student_gradebook_project/users/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+POSSIBLE_GRADES = (
+    ("a", "A"),
+    ("b", "B"),
+    ("c", "C"),
+    ("f", "F"),
+    ("not enrolled", "NOT ENROLLED")
+)
+
 # Create your models here.
 class Student(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
-    Art = models.CharField(max_length=100)
-    Math = models.CharField(max_length=100)
-    History = models.CharField(max_length=100)
-    Literature = models.CharField(max_length=100)
-    Science= models.CharField(max_length=100)
+    Art = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
+    Math = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
+    History = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
+    Literature = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
+    Science= models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
 
     def __str__(self):
-        return f'Student: {self.user.username}'
+        return f'Student: {self.name}'
 
 
 class Teacher(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Teacher: {self.user.username}'
+        return f'Teacher: {self.name}'
