026236e00510757ec448963bece523c687046676
Justin Thomas <thomascjustin@gmail.com>
2019-01-09 18:35:53 -0500

    Update Studet Models

--- student_gradebook_project/users/models.py
+++ student_gradebook_project/users/models.py
@@ -9,9 +9,44 @@ POSSIBLE_GRADES = (
     ("not enrolled", "NOT ENROLLED")
 )
 
+POSSIBLE_RACES = (
+    ("black", "Black or African American"),
+    ("asian", "Asian"),
+    ("latinx", "Latinx"),
+    ("white", "White"),
+    ("amerindian", "American Indian or Alaska Native"),
+    ("hawaiian or pacific islander", "Native Hawaiian or Other Pacific Islander"),
+    ("mixed race", "Mixed Race")
+)
+
+POSSIBLE_SEX = (
+    ("male", "Male"),
+    ("female", "Female"),
+    ("prefer not to answer", "Prefer Not to Answer")
+)
+
+POSSIBLE_GENDER = (
+    ("prefer not to answer", "Prefer Not to Answer"),
+    ("non-binary", "Non-binary/Gender Non-Conforming/Genderqueer"),
+    ("male", "Male"),
+    ("female", "Female")
+)
+
+POSSIBLE_IEP_504_PLAN = (
+    ("iep and 504 plan", "IEP and 504 Plan"),
+    ("none", "None"),
+    ("iep", "IEP"),
+    ("504", "504 Plan")
+)
+
+
 # Create your models here.
 class Student(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE)
+    Sex = models.CharField(max_length=12, choices=POSSIBLE_SEX, default='')
+    Gender = models.CharField(max_length=100, choices=POSSIBLE_GENDER, default='')
+    Race = models.CharField(max_length=100, choices=POSSIBLE_RACES, default='')
+    IEP_or_504_Plan = models.CharField(max_length=100, choices=POSSIBLE_IEP_504_PLAN, default='None')
     Art = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
     Math = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
     History = models.CharField(max_length=12, choices=POSSIBLE_GRADES, default='not enrolled')
